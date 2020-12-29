6b39ba943f6ab99231a586875a88a9f573d96187
Ras1128 <74952140+Ras1128@users.noreply.github.com>
2020-12-30 00:40:10 +0900

    ActiveHashの実装をした

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,17 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def products_params
+    params.require(:product).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,18 @@
 class Product < ApplicationRecord
-
   belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :area
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :shipping_day
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :area_id
+    validates :condition_id
+    validates :postage_id
+    validates :shipping_day_id
+  end
 end
