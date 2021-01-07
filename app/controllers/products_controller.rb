86e6b6701a8cfe49cc6e7aa68acaf8c65084efe5
Ras1128 <74952140+Ras1128@users.noreply.github.com>
2021-01-07 16:45:57 +0900

    商品情報編集機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,22 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def edit
+    @product = Product.find(params[:id])
+    unless current_user.id == @product.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(products_params)
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
   def products_params
     params.require(:product).permit(:name, :description, :price, :category_id, :item_condition_id, :postage_id, :shipping_area_id, :shipping_day_id, :image).merge(user_id: current_user.id)
   end
