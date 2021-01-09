e61803a5c178979251f6836183c8d2f5853c9cd9
Ras1128 <74952140+Ras1128@users.noreply.github.com>
2021-01-09 18:51:13 +0900

    修正１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,8 +36,11 @@ class ProductsController < ApplicationController
 
   def destroy
     product = Product.find(params[:id])
-    product.destroy
-    redirect_to root_path
+    if product.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
 
