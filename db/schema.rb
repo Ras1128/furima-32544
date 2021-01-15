26123d9abc2650d700559071946b5c253bdd30f7
Ras1128 <74952140+Ras1128@users.noreply.github.com>
2021-01-15 18:57:11 +0900

    マイグレーションファイルの変更

--- db/schema.rb
+++ db/schema.rb
@@ -66,8 +66,8 @@ ActiveRecord::Schema.define(version: 2021_01_11_105736) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_transactions_on_product_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
+    t.index ["product_id"], name: "index_purchases_on_product_id"
+    t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
